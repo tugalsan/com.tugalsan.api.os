162dac89dac8681a3b9a29209c5b03a90df46e35
tugalsan karabacak <tugalsan@gmail.com>
2023-05-17 08:48:08 +0300

    .

---
+++ src/main/java/com/tugalsan/api/os/server/TS_OsProcessRunning.java
@@ -0,0 +1,7 @@
+package com.tugalsan.api.os.server;
+
+public class TS_OsProcessRunning {
+    
+    public static void a(){
+    }
+}
--- src/main/java/com/tugalsan/api/os/server/TS_OsProcessUtils.java
+++ src/main/java/com/tugalsan/api/os/server/TS_OsProcessUtils.java
@@ -1,5 +1,10 @@
 package com.tugalsan.api.os.server;
 
+import com.tugalsan.api.unsafe.client.TGS_UnSafe;
+import java.lang.ProcessHandle.Info;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class TS_OsProcessUtils {
 
     public static int processorCount() {
@@ -8,5 +13,25 @@ public class TS_OsProcessUtils {
 
     public static long pid() {
         return ProcessHandle.current().pid();
+    }
+
+    public static Info info() {
+        return ProcessHandle.current().info();
+    }
+
+    public static Stream<ProcessHandle> list() {
+        return ProcessHandle.allProcesses();
+    }
+
+    public static boolean runJar(Path jarFile, CharSequence... arguments) {
+        return TGS_UnSafe.call(() -> {
+            var pre = "--enable-preview --add-modules jdk.incubator.concurrent -jar ";
+            var pb = new ProcessBuilder(info().command().get(), pre + "\"" + jarFile.toAbsolutePath().toString() + "\" " + String.join(" ", arguments));
+            pb.inheritIO().start();
+            return true;
+        }, e -> {
+            e.printStackTrace();
+            return false;
+        });
     }
 }
