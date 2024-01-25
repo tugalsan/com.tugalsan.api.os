b5abc8c46ecba70cd17146b8cc5c44108cff18b3
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-26 01:46:57 +0300

    .

--- src/main/java/com/tugalsan/api/os/server/windows/TS_OsWindowsRegistryUtils2.java
+++ src/main/java/com/tugalsan/api/os/server/windows/TS_OsWindowsRegistryUtils2.java
@@ -8,27 +8,27 @@ import java.util.stream.*;
 
 public class TS_OsWindowsRegistryUtils2 {
 
-    public static final int HKEY_CURRENT_USER() {
+    public static int HKEY_CURRENT_USER() {
         return 0x80000001;
     }
 
-    public static final int HKEY_LOCAL_MACHINE() {
+    public static int HKEY_LOCAL_MACHINE() {
         return 0x80000002;
     }
 
-    public static final int REG_SUCCESS() {
+    public static int REG_SUCCESS() {
         return 0;
     }
 
-    public static final int REG_NOTFOUND() {
+    public static int REG_NOTFOUND() {
         return 2;
     }
 
-    public static final int REG_ACCESSDENIED() {
+    public static int REG_ACCESSDENIED() {
         return 5;
     }
 
-    public static final int KEY_WOW64_32KEY() {
+    public static int KEY_WOW64_32KEY() {
         return 0x0200;
     }
 
@@ -36,53 +36,59 @@ public class TS_OsWindowsRegistryUtils2 {
         return 0x0100;
     }
 
-    private static final int KEY_ALL_ACCESS() {
+    private int KEY_ALL_ACCESS() {
         return 0xf003f;
     }
 
-    private static int KEY_READ() {
+    private int KEY_READ() {
         return 0x20019;
     }
-    private static Preferences userRoot = Preferences.userRoot();
-    private static Preferences systemRoot = Preferences.systemRoot();
-    private static Class<? extends Preferences> userClass = userRoot.getClass();
-    private static Method regOpenKey = null;
-    private static Method regCloseKey = null;
-    private static Method regQueryValueEx = null;
-    private static Method regEnumValue = null;
-    private static Method regQueryInfoKey = null;
-    private static Method regEnumKeyEx = null;
-    private static Method regCreateKeyEx = null;
-    private static Method regSetValueEx = null;
-    private static Method regDeleteKey = null;
-    private static Method regDeleteValue = null;
 
-    static {
-        TGS_UnSafe.run(() -> {
-            regOpenKey = userClass.getDeclaredMethod("WindowsRegOpenKey", new Class[]{int.class, byte[].class, int.class});
-            regOpenKey.setAccessible(true);
-            regCloseKey = userClass.getDeclaredMethod("WindowsRegCloseKey", new Class[]{int.class});
-            regCloseKey.setAccessible(true);
-            regQueryValueEx = userClass.getDeclaredMethod("WindowsRegQueryValueEx", new Class[]{int.class, byte[].class});
-            regQueryValueEx.setAccessible(true);
-            regEnumValue = userClass.getDeclaredMethod("WindowsRegEnumValue", new Class[]{int.class, int.class, int.class});
-            regEnumValue.setAccessible(true);
-            regQueryInfoKey = userClass.getDeclaredMethod("WindowsRegQueryInfoKey1", new Class[]{int.class});
-            regQueryInfoKey.setAccessible(true);
-            regEnumKeyEx = userClass.getDeclaredMethod("WindowsRegEnumKeyEx", new Class[]{int.class, int.class, int.class});
-            regEnumKeyEx.setAccessible(true);
-            regCreateKeyEx = userClass.getDeclaredMethod("WindowsRegCreateKeyEx", new Class[]{int.class, byte[].class});
-            regCreateKeyEx.setAccessible(true);
-            regSetValueEx = userClass.getDeclaredMethod("WindowsRegSetValueEx", new Class[]{int.class, byte[].class, byte[].class});
-            regSetValueEx.setAccessible(true);
-            regDeleteValue = userClass.getDeclaredMethod("WindowsRegDeleteValue", new Class[]{int.class, byte[].class});
-            regDeleteValue.setAccessible(true);
-            regDeleteKey = userClass.getDeclaredMethod("WindowsRegDeleteKey", new Class[]{int.class, byte[].class});
-            regDeleteKey.setAccessible(true);
-        });
+    private TS_OsWindowsRegistryUtils2() throws NoSuchMethodException {
+        userRoot = Preferences.userRoot();
+        systemRoot = Preferences.systemRoot();
+        userClass = userRoot.getClass();
+        regOpenKey = userClass.getDeclaredMethod("WindowsRegOpenKey", new Class[]{int.class, byte[].class, int.class});
+        regOpenKey.setAccessible(true);
+        regCloseKey = userClass.getDeclaredMethod("WindowsRegCloseKey", new Class[]{int.class});
+        regCloseKey.setAccessible(true);
+        regQueryValueEx = userClass.getDeclaredMethod("WindowsRegQueryValueEx", new Class[]{int.class, byte[].class});
+        regQueryValueEx.setAccessible(true);
+        regEnumValue = userClass.getDeclaredMethod("WindowsRegEnumValue", new Class[]{int.class, int.class, int.class});
+        regEnumValue.setAccessible(true);
+        regQueryInfoKey = userClass.getDeclaredMethod("WindowsRegQueryInfoKey1", new Class[]{int.class});
+        regQueryInfoKey.setAccessible(true);
+        regEnumKeyEx = userClass.getDeclaredMethod("WindowsRegEnumKeyEx", new Class[]{int.class, int.class, int.class});
+        regEnumKeyEx.setAccessible(true);
+        regCreateKeyEx = userClass.getDeclaredMethod("WindowsRegCreateKeyEx", new Class[]{int.class, byte[].class});
+        regCreateKeyEx.setAccessible(true);
+        regSetValueEx = userClass.getDeclaredMethod("WindowsRegSetValueEx", new Class[]{int.class, byte[].class, byte[].class});
+        regSetValueEx.setAccessible(true);
+        regDeleteValue = userClass.getDeclaredMethod("WindowsRegDeleteValue", new Class[]{int.class, byte[].class});
+        regDeleteValue.setAccessible(true);
+        regDeleteKey = userClass.getDeclaredMethod("WindowsRegDeleteKey", new Class[]{int.class, byte[].class});
+        regDeleteKey.setAccessible(true);
     }
+    private final Preferences userRoot;
+    private final Preferences systemRoot;
+    private final Class<? extends Preferences> userClass;
+    private final Method regOpenKey;
+    private final Method regCloseKey;
+    private final Method regQueryValueEx;
+    private final Method regEnumValue;
+    private final Method regQueryInfoKey;
+    private final Method regEnumKeyEx;
+    private final Method regCreateKeyEx;
+    private final Method regSetValueEx;
+    private final Method regDeleteKey;
+    private final Method regDeleteValue;
 
-    private TS_OsWindowsRegistryUtils2() {
+    public static Optional<TS_OsWindowsRegistryUtils2> of() {
+        return TGS_UnSafe.call(() -> Optional.of(new TS_OsWindowsRegistryUtils2()),
+                 e -> {
+                    e.printStackTrace();
+                    return Optional.empty();
+                });
     }
 
     /**
@@ -97,7 +103,7 @@ public class TS_OsWindowsRegistryUtils2 {
      * @return the value
      *
      */
-    public static String readString(int hkey, CharSequence key, CharSequence valueName, int wow64) {
+    public String readString(int hkey, CharSequence key, CharSequence valueName, int wow64) {
         return TGS_UnSafe.call(() -> {
             if (Objects.equals(hkey, HKEY_LOCAL_MACHINE())) {
                 return readString(systemRoot, hkey, key, valueName, wow64);
@@ -119,7 +125,7 @@ public class TS_OsWindowsRegistryUtils2 {
      * registry view, or KEY_WOW64_64KEY to force access to 64-bit registry view
      * @return the value name(s) plus the value(s)
      */
-    public static Map<String, String> readStringValues(int hkey, String key, int wow64) {
+    public Map<String, String> readStringValues(int hkey, String key, int wow64) {
         return TGS_UnSafe.call(() -> {
             if (Objects.equals(hkey, HKEY_LOCAL_MACHINE())) {
                 return readStringValues(systemRoot, hkey, key, wow64);
@@ -141,7 +147,7 @@ public class TS_OsWindowsRegistryUtils2 {
      * registry view, or KEY_WOW64_64KEY to force access to 64-bit registry view
      * @return the value name(s)
      */
-    public static List<String> readStringSubKeys(int hkey, String key, int wow64) {
+    public List<String> readStringSubKeys(int hkey, String key, int wow64) {
         return TGS_UnSafe.call(() -> {
             if (Objects.equals(hkey, HKEY_LOCAL_MACHINE())) {
                 return readStringSubKeys(systemRoot, hkey, key, wow64);
@@ -159,7 +165,7 @@ public class TS_OsWindowsRegistryUtils2 {
      * @param hkey HKEY_CURRENT_USER/HKEY_LOCAL_MACHINE
      * @param key
      */
-    public static void createKey(int hkey, String key) {
+    public void createKey(int hkey, String key) {
         TGS_UnSafe.run(() -> {
             int[] ret;
             if (Objects.equals(hkey, HKEY_LOCAL_MACHINE())) {
@@ -191,7 +197,7 @@ public class TS_OsWindowsRegistryUtils2 {
      * 64-bits for 64-bits app) or KEY_WOW64_32KEY to force access to 32-bit
      * registry view, or KEY_WOW64_64KEY to force access to 64-bit registry view
      */
-    public static void writeStringValue(int hkey, CharSequence key, CharSequence valueName, CharSequence value, int wow64) {
+    public void writeStringValue(int hkey, CharSequence key, CharSequence valueName, CharSequence value, int wow64) {
         TGS_UnSafe.run(() -> {
             if (Objects.equals(hkey, HKEY_LOCAL_MACHINE())) {
                 writeStringValue(systemRoot, hkey, key, valueName, value, wow64);
@@ -211,7 +217,7 @@ public class TS_OsWindowsRegistryUtils2 {
      * @param hkey
      * @param key
      */
-    public static void deleteKey(int hkey, CharSequence key) {
+    public void deleteKey(int hkey, CharSequence key) {
         TGS_UnSafe.run(() -> {
             var rc = -1;
             if (Objects.equals(hkey, HKEY_LOCAL_MACHINE())) {
@@ -236,7 +242,7 @@ public class TS_OsWindowsRegistryUtils2 {
      * 64-bits for 64-bits app) or KEY_WOW64_32KEY to force access to 32-bit
      * registry view, or KEY_WOW64_64KEY to force access to 64-bit registry view
      */
-    public static void deleteValue(int hkey, CharSequence key, CharSequence value, int wow64) {
+    public void deleteValue(int hkey, CharSequence key, CharSequence value, int wow64) {
         TGS_UnSafe.run(() -> {
             var rc = -1;
             if (Objects.equals(hkey, HKEY_LOCAL_MACHINE())) {
@@ -251,7 +257,7 @@ public class TS_OsWindowsRegistryUtils2 {
     }
 
     //========================================================================
-    private static int deleteValue(Preferences root, int hkey, CharSequence key, CharSequence value, int wow64) {
+    private int deleteValue(Preferences root, int hkey, CharSequence key, CharSequence value, int wow64) {
         return TGS_UnSafe.call(() -> {
             var handles = (int[]) regOpenKey.invoke(root, new Object[]{
                 hkey, toCstr(key), KEY_ALL_ACCESS() | wow64});
@@ -267,17 +273,16 @@ public class TS_OsWindowsRegistryUtils2 {
     }
 
     //========================================================================
-    private static int deleteKey(Preferences root, int hkey, CharSequence key) {
+    private int deleteKey(Preferences root, int hkey, CharSequence key) {
         return TGS_UnSafe.call(() -> {
-            var rc = ((Integer) regDeleteKey.invoke(root, new Object[]{
+            return ((Integer) regDeleteKey.invoke(root, new Object[]{
                 hkey, toCstr(key)
-            }));
-            return rc;  // can REG_NOTFOUND, REG_ACCESSDENIED, REG_SUCCESS
+            }));// can REG_NOTFOUND, REG_ACCESSDENIED, REG_SUCCESS
         });
     }
 
     //========================================================================
-    private static String readString(Preferences root, int hkey, CharSequence key, CharSequence value, int wow64) {
+    private String readString(Preferences root, int hkey, CharSequence key, CharSequence value, int wow64) {
         return TGS_UnSafe.call(() -> {
             var handles = (int[]) regOpenKey.invoke(root, new Object[]{
                 hkey, toCstr(key), KEY_READ() | wow64});
@@ -293,7 +298,7 @@ public class TS_OsWindowsRegistryUtils2 {
     }
 
     //========================================================================
-    private static Map<String, String> readStringValues(Preferences root, int hkey, CharSequence key, int wow64) {
+    private Map<String, String> readStringValues(Preferences root, int hkey, CharSequence key, int wow64) {
         return TGS_UnSafe.call(() -> {
             var results = new HashMap<String, String>();
             var handles = (int[]) regOpenKey.invoke(root, new Object[]{
@@ -319,7 +324,7 @@ public class TS_OsWindowsRegistryUtils2 {
     }
 
     //========================================================================
-    private static List<String> readStringSubKeys(Preferences root, int hkey, CharSequence key, int wow64) {
+    private List<String> readStringSubKeys(Preferences root, int hkey, CharSequence key, int wow64) {
         return TGS_UnSafe.call(() -> {
             List<String> results = new ArrayList();
             var handles = (int[]) regOpenKey.invoke(root, new Object[]{
@@ -347,7 +352,7 @@ public class TS_OsWindowsRegistryUtils2 {
     }
 
     //========================================================================
-    private static int[] createKey(Preferences root, int hkey, CharSequence key) {
+    private int[] createKey(Preferences root, int hkey, CharSequence key) {
         return TGS_UnSafe.call(() -> {
             return (int[]) regCreateKeyEx.invoke(root, new Object[]{
                 hkey, toCstr(key)
@@ -356,7 +361,7 @@ public class TS_OsWindowsRegistryUtils2 {
     }
 
     //========================================================================
-    private static void writeStringValue(Preferences root, int hkey, CharSequence key, CharSequence valueName, CharSequence value, int wow64) {
+    private void writeStringValue(Preferences root, int hkey, CharSequence key, CharSequence valueName, CharSequence value, int wow64) {
         TGS_UnSafe.run(() -> {
             var handles = (int[]) regOpenKey.invoke(root, new Object[]{
                 hkey, toCstr(key), KEY_ALL_ACCESS() | wow64});
@@ -369,7 +374,7 @@ public class TS_OsWindowsRegistryUtils2 {
 
     //========================================================================
     // utility
-    private static byte[] toCstr(CharSequence str) {
+    private byte[] toCstr(CharSequence str) {
         return TGS_UnSafe.call(() -> {
             var result = new byte[str.length() + 1];
             for (var i = 0; i < str.length(); i++) {
