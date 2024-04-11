afb7378f73422dc2c336faac62c9a5f85a709c08
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-11 10:01:41 +0300

    Update TS_OsProcess.java

--- src/main/java/com/tugalsan/api/os/server/TS_OsProcess.java
+++ src/main/java/com/tugalsan/api/os/server/TS_OsProcess.java
@@ -4,6 +4,7 @@ import com.tugalsan.api.list.server.TS_ListCastUtils;
 import com.tugalsan.api.random.server.TS_RandomUtils;
 import com.tugalsan.api.string.client.TGS_StringUtils;
 import com.tugalsan.api.time.server.TS_TimeElapsed;
+import com.tugalsan.api.union.client.TGS_UnionExcuseVoid;
 import com.tugalsan.api.union.server.TS_UnionUtils;
 import java.io.BufferedReader;
 import java.io.File;
@@ -17,6 +18,16 @@ import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 public class TS_OsProcess {
+
+    public TGS_UnionExcuseVoid toUnion() {
+        if (exception != null) {
+            return TGS_UnionExcuseVoid.ofExcuse(exception);
+        }
+        if (!exitValueOk()) {
+            return TGS_UnionExcuseVoid.ofExcuse(TS_OsProcess.class.getSimpleName(), "toUnion", "!exitValueOk()");
+        }
+        return TGS_UnionExcuseVoid.ofVoid();
+    }
 
     public static Path executionPath() {
         return Path.of(System.getProperty("user.dir"));
